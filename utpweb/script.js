30605dbc188c680ae16206fa32da44898e7a6c83
zainabaqilah <112572182+zainabaqilah@users.noreply.github.com>
2024-05-21 14:46:31 +0700

    Add files via upload

---
+++ utpweb/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Grapichs Designer", "Website Developper"],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1000,
+    loop: true
+})
